23fb8d7223618642b32f722ee355659aaa3e6004
hushchynd <hushchynd@gmail.com>
2023-05-30 16:41:36 +0300

    admin frontend complete

---
+++ admin_panel/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+import admin_panel.views
+
+urlpatterns = [
+    path("statistic", admin_panel.views.statistic, name='statistic'),
+    path("paybox", admin_panel.views.paybox, name='paybox'),
+    path("receipt", admin_panel.views.receipt, name='receipt'),
+    path("personal_account", admin_panel.views.personal_account, name='personal_account'),
+    path("flat", admin_panel.views.flat, name='flat'),
+    path("client", admin_panel.views.client, name='client'),
+    path("house", admin_panel.views.house, name='house'),
+    path("message", admin_panel.views.message, name='message'),
+    path("application", admin_panel.views.application, name='application'),
+    path("indications", admin_panel.views.indications, name='indications'),
+    path("system_services", admin_panel.views.system_services, name='system_services'),
+    path("system_tarrifs", admin_panel.views.system_tarrifs, name='system_tarrifs'),
+    path("system_users", admin_panel.views.system_users, name='system_users'),
+    path("system_payment_details", admin_panel.views.system_payment_details, name='system_payment_details'),
+    path("system_payment_articles", admin_panel.views.system_payment_articles, name='system_payment_articles'),
+
+]
---
+++ admin_panel/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+
+
+def statistic(request):
+    return render(request, 'admin_panel/statistic.html')
+
+
+def paybox(request):
+    return render(request, 'admin_panel/paybox.html')
+
+
+def receipt(request):
+    return render(request, 'admin_panel/receipt.html')
+
+
+def personal_account(request):
+    return render(request, 'admin_panel/personal_account.html')
+
+
+def flat(request):
+    return render(request, 'admin_panel/flat.html')
+
+
+def client(request):
+    return render(request, 'admin_panel/clients.html')
+
+
+def house(request):
+    return render(request, 'admin_panel/house.html')
+
+
+def message(request):
+    return render(request, 'admin_panel/message.html')
+
+
+def application(request):
+    return render(request, 'admin_panel/application.html')
+
+
+def indications(request):
+    return render(request, 'admin_panel/indications.html')
+
+
+def system_services(request):
+    return render(request, 'admin_panel/system_settings/services.html')
+
+
+def system_tarrifs(request):
+    return render(request, 'admin_panel/system_settings/tariffs.html')
+
+
+def system_users(request):
+    return render(request, 'admin_panel/system_settings/users.html')
+
+
+def system_payment_details(request):
+    return render(request, 'admin_panel/system_settings/payment_details.html')
+
+
+def system_payment_articles(request):
+    return render(request, 'admin_panel/system_settings/payment_articles.html')
